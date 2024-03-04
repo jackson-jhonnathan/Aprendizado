ba0534ad2b67479894f45b40f1b20fa4432092ed
Jackson Jhonnathan <131427349+jackson-jhonnathan@users.noreply.github.com>
2024-03-03 21:11:23 -0300

    entrada_saida.py

---
+++ entrada_saida.py
@@ -0,0 +1,9 @@
+print('Olá, Seja Bem-Vindo ao meu primeiro programa!')
+print()
+entrada = input('[E]ntrar ou [S]air ')
+print(entrada)
+if entrada == 'E':
+    print('Você entrou no sistema.')
+elif entrada == 'S':
+    print('Você saiu do sistema.')
+    
